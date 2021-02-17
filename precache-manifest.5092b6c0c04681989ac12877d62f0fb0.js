ee11d3930f44b86c015542621cb665e910008426
akjha96 <akjha96@gmail.com>
2021-02-17 10:45:59 +0530

    Updates

---
+++ precache-manifest.5092b6c0c04681989ac12877d62f0fb0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cced06917deaae0670e50d8b593b388",
+    "url": "/acrDevelopmentClone/index.html"
+  },
+  {
+    "revision": "a1701a70b953a958bf67",
+    "url": "/acrDevelopmentClone/static/js/2.75103bee.chunk.js"
+  },
+  {
+    "revision": "14c54f8d621b663941f58f815237ec03",
+    "url": "/acrDevelopmentClone/static/js/2.75103bee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "890893fe611dd17b872d",
+    "url": "/acrDevelopmentClone/static/js/main.344f0977.chunk.js"
+  },
+  {
+    "revision": "2c96237af5c7bdb8fbaf",
+    "url": "/acrDevelopmentClone/static/js/runtime-main.dd6968cf.js"
+  },
+  {
+    "revision": "350387f5ee9e1569dc00c4952fca7cc8",
+    "url": "/acrDevelopmentClone/static/media/Custom Software Icon.350387f5.svg"
+  },
+  {
+    "revision": "e227d87b2dec28a248985906d655c9ed",
+    "url": "/acrDevelopmentClone/static/media/Footer Adornment.e227d87b.svg"
+  },
+  {
+    "revision": "c95af783da234448e6b522bd80339aee",
+    "url": "/acrDevelopmentClone/static/media/facebook.c95af783.svg"
+  },
+  {
+    "revision": "8a1f257287cb0615e1a97abefedcaaac",
+    "url": "/acrDevelopmentClone/static/media/instagram.8a1f2572.svg"
+  },
+  {
+    "revision": "ea0de06c0dcabac997a9b8f5605ee00b",
+    "url": "/acrDevelopmentClone/static/media/logo.ea0de06c.svg"
+  },
+  {
+    "revision": "6838a9fb85f01988c85e38475ae59fb0",
+    "url": "/acrDevelopmentClone/static/media/mobileIcon.6838a9fb.svg"
+  },
+  {
+    "revision": "13b0990004faa8299dd60e83b2b12569",
+    "url": "/acrDevelopmentClone/static/media/twitter.13b09900.svg"
+  },
+  {
+    "revision": "6b90d7dca8c7d0d5340cc0748c6bdb3c",
+    "url": "/acrDevelopmentClone/static/media/websiteIcon.6b90d7dc.svg"
+  }
+]);
